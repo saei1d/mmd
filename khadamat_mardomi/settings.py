0c77360ccd41fffae36826dd9ea52dfd7addbd5c
saei1d <hehehe3bar@gmail.com>
2023-10-26 13:03:56 +0330

    init

--- khadamat_mardomi/settings.py
+++ khadamat_mardomi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'khadamat_mardomi.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("mmd"),
+        "USER": os.getenv("postgres"),
+        "PASSWORD": os.getenv("LPTuGhCQ5lEvUoF8N8mwey1IqsnJ2Tpz"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("5432"),
     }
 }
 
